0418beee8ab4c5b8c4b7a287197c05bbd2eddff1
william <william24jes@hotmail.com>
2024-04-04 18:06:57 -0500

    cambio de color listo

--- app/src/main/java/com/example/lab1/MainActivity.java
+++ app/src/main/java/com/example/lab1/MainActivity.java
@@ -19,11 +19,37 @@ import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
+    private ImageView calculatorLogo;
+    private TypedArray randomImages;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
 
+        TextView telemathTextView = findViewById(R.id.Telemath);
+        calculatorLogo = findViewById(R.id.calculatorLogo);
+        randomImages = getResources().obtainTypedArray(R.array.random_images);
 
+        telemathTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Obtener una imagen al azar
+                Random random = new Random();
+                int randomIndex = random.nextInt(randomImages.length());
+                int randomImageId = randomImages.getResourceId(randomIndex, -1);
 
-}
+                if (randomImageId != -1) {
+                    calculatorLogo.setImageResource(randomImageId);
+                }
+            }
+        });
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        randomImages.recycle();
+    }
 
 }
